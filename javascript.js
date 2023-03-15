22dc76c7f7863875c2e30d46c215ae7d29c284c3
yiechernchuen <yccgithub@gmail.com>
2023-03-15 16:14:16 +0800

    Add mouseover event, randomColorGenerator function & container border

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+// const button = document.createElement('button');
+// button.classList.add('button');
+// document.body.appendChild(button);
+
 const container = document.createElement('div');
 container.classList.add('container');
 document.body.append(container);
@@ -7,3 +11,16 @@ for (let i = 1; i <= 256; i++) {
     div.classList.add('box');
     document.querySelector('.container').append(div);
 }
+
+const box = document.querySelectorAll('.box');
+
+box.forEach(mouseOver);
+
+function mouseOver(box) {
+    box.addEventListener('mouseover', (e) => (e.target.style.backgroundColor = randomColorGenerator()), { once: true });
+}
+
+const randomColorGenerator = () =>
+    `#${Math.floor(Math.random() * 16777216)
+        .toString(16)
+        .padStart(6, 0)}`;
