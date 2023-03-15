b90b05476aa26cd313691846f9dce7229dc67a0d
yiechernchuen <yccgithub@gmail.com>
2023-03-15 17:45:55 +0800

    Add a prompt button & userInputSquares function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
-// const button = document.createElement('button');
-// button.classList.add('button');
-// document.body.appendChild(button);
+const button = document.createElement('button');
+button.classList.add('button');
+document.body.appendChild(button);
+button.textContent = 'Grid Size';
 
 const container = document.createElement('div');
 container.classList.add('container');
@@ -13,14 +14,31 @@ for (let i = 1; i <= 256; i++) {
 }
 
 const box = document.querySelectorAll('.box');
-
-box.forEach(mouseOver);
-
-function mouseOver(box) {
+box.forEach((box) => {
     box.addEventListener('mouseover', (e) => (e.target.style.backgroundColor = randomColorGenerator()), { once: true });
-}
+});
 
 const randomColorGenerator = () =>
     `#${Math.floor(Math.random() * 16777216)
         .toString(16)
         .padStart(6, 0)}`;
+
+const userInputSquares = () => {
+    let input = prompt('Please input number of squares per side for the new grid');
+    const elementToBeRemoved = document.querySelectorAll('.box');
+    elementToBeRemoved.forEach((element) => element.remove());
+    for (let i = 1; i <= input * input; i++) {
+        const div = document.createElement('div');
+        div.classList.add('box');
+        document.querySelector('.container').append(div);
+    }
+    const box = document.querySelectorAll('.box');
+    box.forEach((box) => {
+        box.style.flex = `1 0 ${800 / input}px`;
+        box.addEventListener('mouseover', (e) => (e.target.style.backgroundColor = randomColorGenerator()), {
+            once: true,
+        });
+    });
+};
+
+button.addEventListener('click', userInputSquares);
