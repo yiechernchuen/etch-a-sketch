180adf43808b4ef4c8b9f4f176adc096f4178750
yiechernchuen <yccgithub@gmail.com>
2023-03-17 18:06:38 +0800

    Improve code readability

--- javascript.js
+++ javascript.js
@@ -20,27 +20,50 @@ button[4].textContent = 'Color Picker: ';
 
 const colorPicker = document.createElement('input');
 colorPicker.setAttribute('type', 'color');
+colorPicker.setAttribute('class', 'colorPicker1');
 button[4].classList.add('colorPicker');
 button[4].appendChild(colorPicker);
+
+// const darkenFactor = document.createElement('input');
+// darkenFactor.setAttribute('type', 'range');
+// darkenFactor.setAttribute('min', '0');
+// darkenFactor.setAttribute('max', '1');
+// darkenFactor.setAttribute('step', '0.1');
+// darkenFactor.setAttribute('value', '0');
+// darkenFactor.setAttribute('class', 'darkenFactor1');
+// button[5].classList.add('darkenFactor');
+// const span = document.createElement('span');
+// button[5].appendChild(span);
+// span.textContent = `Darken Factor: ${darkenFactor.value}`;
+// button[5].appendChild(darkenFactor);
 
 const container = document.createElement('div');
 container.classList.add('container');
 document.body.append(container);
 
 const randomColorGenerator = () =>
-    `#${Math.floor(Math.random() * 16777216)
+    Math.floor(Math.random() * 16777216)
         .toString(16)
-        .padStart(6, 0)}`;
+        .padStart(6, 0);
 
 for (let i = 1; i <= 16 * 16; i++) {
     const div = document.createElement('div');
     div.classList.add('box');
     document.querySelector('.container').append(div);
 }
-
+const rainbowBackground = (e) => {
+    e.target.style.backgroundColor = `#${randomColorGenerator()}`;
+};
+const eraseBackground = (e) => {
+    e.target.style.backgroundColor = 'white';
+};
+const colorPickerBackground = (e) => {
+    const selectedColor = document.querySelector('.colorPicker1').value;
+    e.target.style.backgroundColor = selectedColor;
+};
 let box = document.querySelectorAll('.box');
 box.forEach((box) => {
-    box.addEventListener('mouseover', (e) => (e.target.style.backgroundColor = randomColorGenerator()));
+    box.addEventListener('mouseover', rainbowBackground);
 });
 
 const userInputSquares = () => {
@@ -65,13 +88,15 @@ const userInputSquares = () => {
     box = document.querySelectorAll('.box');
     box.forEach((box) => {
         box.style.flex = `1 0 ${800 / input}px`;
-        box.addEventListener('mouseover', (e) => (e.target.style.backgroundColor = randomColorGenerator()));
+        box.addEventListener('mouseover', rainbowBackground);
     });
 };
 
 button[0].addEventListener('click', () => {
     box.forEach((box) => {
-        box.addEventListener('mouseover', (e) => (e.target.style.backgroundColor = randomColorGenerator()));
+        box.removeEventListener('mouseover', eraseBackground);
+        box.removeEventListener('mouseover', colorPickerBackground);
+        box.addEventListener('mouseover', rainbowBackground);
     });
 });
 
@@ -79,7 +104,9 @@ button[1].addEventListener('click', userInputSquares);
 
 button[2].addEventListener('click', () =>
     box.forEach((box) => {
-        box.addEventListener('mouseover', (e) => (e.target.style.backgroundColor = 'white'));
+        box.removeEventListener('mouseover', rainbowBackground);
+        box.removeEventListener('mouseover', colorPickerBackground);
+        box.addEventListener('mouseover', eraseBackground);
     })
 );
 
@@ -91,9 +118,20 @@ button[3].addEventListener('click', () => {
 
 ['click', 'input'].forEach((e) =>
     colorPicker.addEventListener(e, () => {
-        const selectedColor = document.querySelector('input').value;
         box.forEach((box) => {
-            box.addEventListener('mouseover', (e) => (e.target.style.backgroundColor = selectedColor));
+            box.removeEventListener('mouseover', rainbowBackground);
+            box.removeEventListener('mouseover', eraseBackground);
+            box.addEventListener('mouseover', colorPickerBackground);
         });
     })
 );
+
+// darkenFactor.addEventListener('input', () => {
+//     span.textContent = `Darken Factor: ${darkenFactor.value}`;
+//     box.forEach((box) => {
+//         box.removeEventListener('mouseover', rainbowBackground);
+//     });
+//     box.forEach((box) => {
+//         box.addEventListener('mouseover', rainbowBackground, { once: true });
+//     });
+// });
